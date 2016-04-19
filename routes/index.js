21a9d2eda88002bb900ff58495a337dbe6bcee4e
Rachelle Gray <msrachellegray@gmail.com>
2016-04-18 20:12:38 -0700

    canpa - callback sends mysql dump object length  to template

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,23 @@
  */
 exports.index = function(req, res){
 
-	var initialQuery = 'SELECT * FROM plant';
-	db.query(initialQuery, function(err, result) {
-		if(err) {
-			console.log(err);
-		} else {
-			console.log(result);
-		}
-	});
-	res.render('index.html');
+	function loadDBQuery(callback) {
+		var initialQuery = 'SELECT * FROM plant';
+		db.query(initialQuery, function(err, result) {
+			if(err) {
+				console.log(err);
+			} else {
+				//tableDump = result;
+				resOptions(result);
+			}
+		});
+	}
+
+	function resOptions(tableDump) {
+		console.log(tableDump);
+		res.locals = {result: tableDump.length + ' plant results'};
+		res.render('index.html');
+	}
+
+	loadDBQuery(resOptions);
 };
