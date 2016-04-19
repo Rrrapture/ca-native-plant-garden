04ccd0ac2e3fc6697846bda09e17d93766412d8d
Rachelle Gray <msrachellegray@gmail.com>
2016-04-18 20:47:26 -0700

    canpa - stringify common name and scientific name objects

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,11 @@
 exports.index = function(req, res){
 
 	function loadDBQuery(callback) {
-		var initialQuery = 'SELECT * FROM plant';
+		var initialQuery = 'SELECT common_name, scientific_name FROM plant';
 		db.query(initialQuery, function(err, result) {
 			if(err) {
 				console.log(err);
 			} else {
-				//tableDump = result;
 				resOptions(result);
 			}
 		});
@@ -17,7 +16,8 @@ exports.index = function(req, res){
 
 	function resOptions(tableDump) {
 		console.log(tableDump);
-		res.locals = {result: tableDump.length + ' plant results'};
+		//res.locals = {result: tableDump.length + ' plant results'};
+		res.locals = {result: JSON.stringify(tableDump)};
 		res.render('index.html');
 	}
 
